77d34d8fca506a8347a248307b9b59ff533bdb98
Tamilvendan05 <tamil10190@gmail.com>
2024-01-27 17:26:20 +0530

    Initial Commit

---
+++ 2024JavaWorkout/src/org/java/firstprogram.java
@@ -0,0 +1,22 @@
+package org.java;
+
+public class firstprogram {
+	
+	public void java () {
+		System.out.println("This is first program for Java ");
+	}
+	
+	public void selenium () {
+		System.out.println("This is first program for Selenium");
+	}
+
+	public static void main(String[] args) {
+		
+		firstprogram f = new firstprogram ();
+		f.java();
+		f.selenium();
+		
+
+	}
+
+}
